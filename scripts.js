3014a6c64a2070b2f6f96c216e41f27bac44cda8
Letícia Freitas <leticiadev24@gmail.com>
2024-11-27 13:19:50 -0300

    itens icons

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
+
+// Seleciona os elementos da lista
+const expenseList = document.querySelector("ul")
 
 // Capurando o evento de input para formatar o valor
 amount.oninput = () => {
@@ -24,3 +28,46 @@ function formatCurrencyBRL(value){
 
     return value
 }
+
+// Captura o evento de submit do formulário para obter os valores
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de recarregar a página
+    event.preventDefault()
+
+    // Cria um objeto com os detalhes da nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value, //ícone
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+    // Chama a função que irá adicionar o item na lista
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+        // Cria o elemento para adicionar o item(li) na lista(ul)
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+
+        // Cria o ícone da categoria
+        const expenseIcon = document.createElement("img")
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.category_name)
+
+
+        // Adiciona as informações no item:
+        expenseItem.append(expenseIcon)
+        // Adiciona o item na lista:
+        expenseList.append(expenseItem)
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
+}
+
+
