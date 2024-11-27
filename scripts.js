8a6b1b6b202f6e73ba28f98b07da4224869353c1
Letícia Freitas <leticiadev24@gmail.com>
2024-11-27 12:45:43 -0300

    setting value to BRL

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+// Capurando o evento de input para formatar o valor
 amount.oninput = () => {
+    // Obtém o valor atual do input e remove os caracteres não numéricos
     let value = amount.value.replace(/\D/g, "")
-    amount.value = value
-    console.log(value)
+
+    // Transforma o valor em centávos (exemplo: 150/100 = 1.5, equivalente a R$1,50)
+    value = Number(value)/100
+
+    // Atualiza o valor do input
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+    // Formata o valor no padrão de Real Brasileiro
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+    return value
 }
