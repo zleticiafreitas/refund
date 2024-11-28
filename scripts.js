7fdf4bf458258aaa715b97321e324654ba0d19c0
Letícia Freitas <leticiadev24@gmail.com>
2024-11-28 12:44:22 -0300

    finished

--- scripts.js
+++ scripts.js
@@ -94,6 +94,8 @@ function expenseAdd(newExpense) {
         expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
         // Adiciona o item na lista:
         expenseList.append(expenseItem)
+        //Limpa o formulário para adicionar um novo item
+        formClear()
         // Atualiza os totais
         updateTotals()
     } catch (error) {
@@ -144,4 +146,25 @@ function updateTotals() {
     }
 }
 
+// Evento que captura o clique nos itens da lista
+expenseList.addEventListener("click", function (event){
+    // Verifica se o elemento clicado é o ícone de remover
+    if(event.target.classList.contais("remove-icon")){
+        // Obtém a li pai do elemento clicado
+        const item = event.target.closest(".expense")
+        // Remove item da lista.
+        item.remove()
+    }
+    // Atualiza os totais.
+    updateTotals()
+})
 
+function formClear(){
+    // Limpa os inputs
+    expense.value = ""
+    category.value = ""
+    amount.value = ""
+    
+    // Coloca o foco no input do nome da despesa
+    expense.focus()
+}
